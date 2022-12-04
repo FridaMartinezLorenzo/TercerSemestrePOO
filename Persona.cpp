42173e5b5246de72ebd2262781a5d8b5f54b9918
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2022-12-04 13:02:55 -0600

    Persona en c++ (Herencia)

--- Persona.cpp
+++ Persona.cpp
@@ -123,29 +123,40 @@ void Administrativo :: show (){
 
 //_________________________________________________________________________________________
 int main (){
+    Persona i1;
     Estudiante s1;
     Profesor p1;
     Administrativo a1;
+	
+    //Rellenamos para Persona
+    i1.setName("Adrik");
+    i1.setSex('M');
+    i1.setAge(19);
+    i1.show();
+	
     //Rellenamos para estudiante
     s1.setName("Fri");//Se puede acceder a los métodos del padre pero no alterar los datos en caso de ser privados
     s1.setSex('F');
     s1.setAge(19);
     s1.setProm(9);
-	s1.show();
+    s1.show();
+    s1.Persona::show();//Se prueba que se ejecuta el método del padre en la subclase especializada	
 
     //Rellenamos para profesor
     p1.setName("Mason");//Se puede acceder a los métodos del padre pero no alterar los datos en caso de ser privados
     p1.setSex('M');
     p1.setAge(21);
     p1.setInst("Psiquiatria");
-	p1.show();
+    p1.show();
+    p1.Persona::show();//Se prueba que se ejecuta el método del padre en la subclase especializada	
 
     //Rellenamos para Administrativo
     a1.setName("Jenna");//Se puede acceder a los métodos del padre pero no alterar los datos en caso de ser privados
     a1.setSex('F');
     a1.setAge(34);
     a1.setJob("Depto. de Becas");
-	a1.show();
+    a1.show();
+    a1.Persona::show();//Se prueba que se ejecuta el método del padre en la subclase especializada	
 
     return 0;
 }
